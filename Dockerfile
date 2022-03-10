a7ccbeca97586a2fbbf9fa65f067fb8ce8db6c46
Juan Cedeno <cto@noobsters.net>
2022-03-10 00:22:33 -0500

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM ghcr.io/graalvm/graalvm-ce:latest
+
+LABEL author="jcedeno"
+
+CMD [ "java", "-version" ]
---
+++ bukkit/src/main/java/us/jcedeno/blackbird.java
@@ -0,0 +1,11 @@
+package us.jcedeno;
+
+/**
+ * blackbird
+ */
+public class blackbird  {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
